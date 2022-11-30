f2ecb37e9907ff3222365c1d71d03b483045a48a
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2022-11-30 16:22:30 -0700

    Add project files.

---
+++ RayTracer/Main.cpp
@@ -0,0 +1,23 @@
+#include "Renderer/Renderer.h"
+#include <iostream>
+
+int main(int, char**)
+{
+	Renderer renderer;
+	renderer.Initialize();
+	renderer.CreateWindow(400, 300);
+	bool quit = false;
+	while (!quit)
+	{
+		SDL_Event event;
+		SDL_PollEvent(&event);
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			quit = true;
+			break;
+		}
+	}
+	renderer.Shutdown();
+	return 0;
+}
