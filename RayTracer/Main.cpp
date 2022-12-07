00e86f67223f4364cbc3bab18e92af8a484dc5a2
leo5d1 <80261451+leo5d1@users.noreply.github.com>
2022-12-07 16:26:50 -0700

    metal ball

--- RayTracer/Main.cpp
+++ RayTracer/Main.cpp
@@ -6,15 +6,20 @@
 
 int main(int, char**)
 {
+	const int width = 800;
+	const int height = 600;
+	const int samples = 10;
+
 	Renderer renderer;
 	renderer.Initialize();
-	renderer.CreateWindow(600, 300);
+	renderer.CreateWindow(width, height);
 
-	Canvas canvas(600, 300, renderer);
-	Camera camera({ 0, 1, 2 }, { 0, 0, 0 }, { 0, 1, 0 }, 70.0f, 600 / (float)300);
+	Canvas canvas(width, height, renderer);
+	Camera camera({ 0, 1, 2 }, { 0, 0, 0 }, { 0, 1, 0 }, 70.0f, width / (float)height);
+
 	Scene scene;
-
-	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ 1, 1, 0 })));
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ -1.25f, 0, -1 }, 0.5f, std::make_unique<Lambertian>(color3{ 1, 1, 0 })));
+	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, 0, -1 }, 0.5f, std::make_unique<Metal>(color3{ 1, 1, 1 }, 0.1f)));
 	scene.AddObject(std::make_unique<Sphere>(glm::vec3{ 0, -100.5f, -1 }, 100.0f, std::make_unique<Lambertian>(color3{ 0.2f, 0.2f, 0.2f })));
 
 	bool quit = false;
@@ -39,7 +44,7 @@ int main(int, char**)
 
 		// render scene
 		canvas.Clear({ 0, 0, 0, 1});
-		renderer.Render(canvas, scene, camera, 100);
+		renderer.Render(canvas, scene, camera, samples);
 		canvas.Update();
 
 		renderer.CopyCanvas(canvas);
